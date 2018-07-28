815c30ec1460b7a51a3d5612663fe46f5a81db5a
Jason <Jason@Colins-MacBook-Pro.local>
2018-07-27 14:40:39 -1000

    added more comments

--- linkedList.js
+++ linkedList.js
@@ -24,14 +24,14 @@ function linkedListGenerator(){
       value: element,
       next: null
     };
-    if(!head) {
+    if(!head) { //if there's no head, then there are no nodes, set both head and tail to newly created node
       head = newNode,
       tail = newNode
-    } else {
-      tail.next = newNode; //why tail = newNode, does tail have a .next property? Thought it would be head.next
-      tail = newNode;
+    } else { //if there is a head that exists:
+      tail.next = newNode; //take tail(newNode).next and set it to the newly created node so it connects and adds onto list
+      tail = newNode; //sets the tail to the newly linked node
     }
-    return tail; //why returning tail?
+    return tail; //why returning tail? Think it's just for the test.
   }
   
 const get = function (number) {
@@ -44,8 +44,8 @@ const get = function (number) {
     else {
       let targetNode = head;
       for (let i = 0; i < number; i++) {
-        if(targetNode.next === null) { //where does head get a .next property from?
-          return false;
+        if(targetNode.next === null) { 
+          return false; //doesn't this break out of the get() completely? It won't return targetNode?
         }
         targetNode = targetNode.next;
       }
@@ -91,41 +91,51 @@ const get = function (number) {
 
 
   const remove = function (number) { //Jamie
-    let nodeToRemove = get(number);
+    let currentNodeToRemove = get(number);
     let previousNode = get(number-1);
-    let newNode = get(number+1);
-    if(number < 0 || nodeToRemove === false) {
+    let nextNode = get(number+1);
+    if(number < 0 || currentNodeToRemove === false) { //don't need this < 0 as get already has this case
       return false;
     }
-    else if(number === 0) { //how did you know to start a case for just 0?  
-      head = newNode;
-      nodeToRemove.next = null;
+    else if(number === 0) { //if number == 0, aka if it is the head
+      head = nextNode; //sets head to next node in order to uncouple/delete first node
+      currentNodeToRemove.next = null; //sets the target node's next to null, thus disconnecting and deleting it
+    } 
+    else if(currentNodeToRemove.next === null) { //if it is the last node in the list
+      previousNode.next = null; //set the previous node's next to null in order to uncouple it
+      tail = previousNode; //set the tail to the previous node
     } else {
-      if(nodeToRemove.next === null) {
-        previousNode.next = null;
-        tail = previousNode;
-      } else {
-        previousNode.next = newNode;
-      }
+        if(currentNodeToRemove.next !== null && number !== 0) { //not sure if this if is necessary at all, just the else
+          previousNode.next = nextNode; //removes currentNodeToRemove by linking the previous node with next node
+        }
     }
+    
+    // else {
+    //   if(nodeToRemove.next === null) { //if it's the last node
+    //     previousNode.next = null; //uncouples previous node from last in order to delete
+    //     tail = previousNode; //sets previous node to the tail, in order to make it the new last node
+    //   } else {
+    //     previousNode.next = newNode;
+    //   }
+    // }
   }
 
 
   const insert = (value, number) => {
-    let tarNode = get(number);
-    let preNode = get(number - 1);
-    let myNode = {
+    let targetNode = get(number);
+    let previousNode = get(number - 1);
+    let insertNode = {
       value: value,
       next: null
     };
-    if (!tarNode) {
+    if (!targetNode) {
       return false;
-    } else if (!preNode) {
-      myNode.next = head;
-      head = myNode;
-    } else {
-      myNode.next = tarNode;
-      preNode.next = myNode;
+    } else if (!previousNode) { //you are inserting at the head
+      insertNode.next = head; //linking old head to new
+      head = insertNode; //changing reference from old head to new node
+    } else { //everything else because there is no tail check, since you cannot insert after tail
+      insertNode.next = targetNode; //inserting insertNode right before targetNode by attaching it's .next to targetNode
+      previousNode.next = insertNode; //attaching the previousNode before the inserted node by setting it's .next to insertNode
     }
   };
 
